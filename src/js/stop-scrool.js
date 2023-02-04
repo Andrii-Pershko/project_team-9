959f292996d5c5b6b3e847b43bb5f3001b2bf11e
Andrii-Pershko <andriiinf@gmail.com>
2023-02-04 17:36:14 +0200

    End?

--- src/js/stop-scrool.js
+++ src/js/stop-scrool.js
@@ -4,9 +4,7 @@ elBody.addEventListener('click', e => {
     const checkModal = document.querySelector('.active');
     if (checkModal) {
         elBody.style.overflow = 'hidden';
-        console.log('Active')
     } else {
-        console.log('NO Active');
         elBody.style.overflow = 'auto';
     }
 })
