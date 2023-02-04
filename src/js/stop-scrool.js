f64687d81fea4c7d2bfda07a83b9d730167cee34
Andrii-Pershko <andriiinf@gmail.com>
2023-02-04 16:44:38 +0200

    add No scroll

--- src/js/stop-scrool.js
+++ src/js/stop-scrool.js
@@ -1,1 +1,13 @@
-// js-stop-scroll-buy-now
+const elBody = document.querySelector('body');
+
+elBody.addEventListener('click', e => {
+    const checkModal = document.querySelector('.active');
+    if (checkModal) {
+        elBody.style.overflow = 'hidden';
+        console.log('Active')
+    } else {
+        console.log('NO Active');
+        elBody.style.overflow = 'auto';
+    }
+})
+
